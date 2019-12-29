85eaa519b71500056f00a2cd5ee14a7aa5bda8fc
Kevin Fallas <kevin.fallas.b@gmail.com>
2019-12-28 19:49:54 -0600

    crea clase barraInferior y la pone a trabajar

--- src/simuladorterremotos/vista/VentanaAplicacion.java
+++ src/simuladorterremotos/vista/VentanaAplicacion.java
@@ -5,11 +5,21 @@
  */
 package simuladorterremotos.vista;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Observable;
 import java.util.Observer;
+import javafx.scene.shape.Line;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
 import simuladorterremotos.control.ControlSimulador;
 
 /**
@@ -19,6 +29,7 @@ import simuladorterremotos.control.ControlSimulador;
 public class VentanaAplicacion extends JFrame implements Observer {
 
     private final ControlSimulador gestor;
+    private BarraInferior barraEstado;
 
     public VentanaAplicacion(String titulo, ControlSimulador gestor) {
         super(titulo);
@@ -41,11 +52,29 @@ public class VentanaAplicacion extends JFrame implements Observer {
             }
 
         });
+        //agrega la funcion de seguir el mouse
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                dibujarLineasMouse(e.getX(), e.getY());
+            }
+        });
+        ajustarComponentes(this);
     }
 
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    private void ajustarComponentes(Container c) {
+        c.setLayout(new BorderLayout());
+        //c.add(BorderLayout.CENTER,)
+        c.add(BorderLayout.PAGE_END, barraEstado = new BarraInferior());
     }
 
     public void init() {
@@ -55,5 +84,35 @@ public class VentanaAplicacion extends JFrame implements Observer {
     private void cerrarVentana() {
         gestor.suprimir(this);
         dispose();
+    }
+
+    private void dibujarLineasMouse(Integer x, Integer y) {
+        Line lineaVertical = new Line(x, 0, x + 10, 600);
+        Line lineaHorizontal = new Line(0, y, 800, y + 10);
+        barraEstado.mostrarMensaje(x, y);
+    }
+}
+
+//clase que se encarga de controlar la barra de abajo, donde se muestran las cordenadas
+class BarraInferior extends JPanel {
+
+    private Label etiquetaMensaje = new Label();
+
+    public BarraInferior() {
+        super();
+        configurar();
+    }
+
+    private void configurar() {
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        add(etiquetaMensaje);
+        mostrarMensaje(0, 0);
+    }
+
+
+    public void mostrarMensaje(Integer x, Integer y) {
+        //aqui falta hacer la conversion de cordenadas x,y a cordenadas que pide el profe
+        etiquetaMensaje.setText("x:" + x + " y:" + y);
     }
 }
