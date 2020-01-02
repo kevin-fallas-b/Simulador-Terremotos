d6f3be9ed567efd0a5c8192c295336daf7688487
Kevin Fallas <kevin.fallas.b@gmail.com>
2020-01-02 15:36:33 -0600

    abre mapa y funciona redimension, agrega menu superior

--- src/simuladorterremotos/vista/PanelCentral.java
+++ src/simuladorterremotos/vista/PanelCentral.java
@@ -5,7 +5,20 @@
  */
 package simuladorterremotos.vista;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -14,19 +27,49 @@ import javax.swing.JScrollPane;
  * @author Kevin
  */
 public class PanelCentral extends JPanel {
+
     private JPanel panel;
+    private JLabel fondo;
+    private BufferedImage mapaCr;
+
     public PanelCentral() {
         super();
         configurar();
     }
 
     private void configurar() {
+        try {
+            mapaCr = ImageIO.read(PanelCentral.class.getResourceAsStream("../resource/MapaCR.png"));
+            setPreferredSize(new Dimension(mapaCr.getWidth(), mapaCr.getHeight()));
+        } catch (IOException ex) {
+            Logger.getLogger(PanelCentral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         setLayout(new GridLayout(1, 1));
-        add(new JScrollPane(panel = new JPanel(){
-            
+        add(new JScrollPane(panel = new JPanel() {
+
         }, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS
         ));
+        panel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                dibujarLineas(e.getX(), e.getY());
+            }
+        });
+        fondo = new JLabel(new ImageIcon(mapaCr));
+        fondo.setLayout(null);
+        fondo.setBounds(0, 0, mapaCr.getWidth(), mapaCr.getHeight());
+        panel.add(fondo);
+
+    }
+
+    private void dibujarLineas(Integer x, Integer y) {
+
     }
 
 }
--- src/simuladorterremotos/vista/VentanaAplicacion.java
+++ src/simuladorterremotos/vista/VentanaAplicacion.java
@@ -7,6 +7,8 @@ package simuladorterremotos.vista;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
@@ -15,6 +17,10 @@ import java.util.Observable;
 import java.util.Observer;
 import javafx.scene.shape.Line;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import simuladorterremotos.control.ControlSimulador;
 
 /**
@@ -26,6 +32,13 @@ public class VentanaAplicacion extends JFrame implements Observer {
     private final ControlSimulador gestor;
     private BarraInferior barraInferior;
     private PanelCentral panelCentral;
+    private JMenuBar menuPrincipal;
+    private JMenu menuVentana;
+    private JMenu menuDatos;
+    private JMenu menuArchivo;
+    private JMenuItem itemCambiarArchivo;
+    private JMenuItem itemLimitarVisualizacion;
+    private JMenuItem itemSalir;
 
     public VentanaAplicacion(String titulo, ControlSimulador gestor) {
         super(titulo);
@@ -37,6 +50,7 @@ public class VentanaAplicacion extends JFrame implements Observer {
     private void configurar() {
         setResizable(true);
         setSize(800, 600);
+        setMinimumSize(new Dimension(400, 400));
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -60,11 +74,12 @@ public class VentanaAplicacion extends JFrame implements Observer {
             }
         });
         ajustarComponentes(this);
+        ajustarMenu();
     }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        
     }
 
     private void ajustarComponentes(Container c) {
@@ -73,6 +88,23 @@ public class VentanaAplicacion extends JFrame implements Observer {
         c.add(BorderLayout.PAGE_END, barraInferior = new BarraInferior());
     }
 
+    private void ajustarMenu(){
+        menuPrincipal = new JMenuBar();
+        menuPrincipal.add(menuArchivo = new JMenu("Archivo"));
+        menuPrincipal.add(menuVentana = new JMenu("Ventana"));
+        menuPrincipal.add(menuDatos = new JMenu("Datos"));
+        
+        menuArchivo.add(itemSalir = new JMenuItem("Salir"));
+        menuVentana.add(itemLimitarVisualizacion = new JMenuItem("Ajustar Parametros"));
+        menuDatos.add(itemCambiarArchivo = new JMenuItem("Cambiar Archivo"));
+        
+        itemSalir.addActionListener((ActionEvent e) -> {
+            cerrarVentana();
+        });
+        
+        setJMenuBar(menuPrincipal);
+    }
+    
     public void init() {
         gestor.registrar(this);
     }
